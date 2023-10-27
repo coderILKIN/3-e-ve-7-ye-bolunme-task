d20014c036484a799f0e7edf34cd32128ef667a1
coderILKIN <tu758403c@code.edu.az>
2023-10-27 18:35:02 +0400

    task add

---
+++ 3-e ve 7-ye bolunme/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace _3_e_ve_7_ye_bolunme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* 1ci hal*/
+
+            //Console.WriteLine("Ekrana bir reqem yaz");
+
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //if (n%3 == 0 && n%7 == 0)
+            //{
+            //    Console.WriteLine("N ededi 3-e ve 7-ye bolunur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("N ededi 3-e ve 7-ye bolunmur");
+            //}
+
+
+            /*2ci hal*/
+
+            //Console.WriteLine("---Ekrana bir reqem yaz---");
+
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //if (n < 0)
+            //{
+            //    Console.WriteLine("Musbet eded yaz");
+            //}
+
+            //else if (n == 0)
+            //{
+            //    Console.WriteLine("0 hec bir edede bolunmur");
+            //}
+
+            //else if (n % 3 == 0 && n % 7 == 0)
+            //{
+            //    Console.WriteLine("Eded 3-e ve 7-ye bolunur");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Eded 3-e ve 7-ye bolunmur");
+            //}
+
+
+            /* 3cu hal */
+
+            //Console.WriteLine("---Ekrana 3-e ve 7-ye bolunen bir reqem yaz---");
+
+            //int n;
+
+            //for (int i = 1; i > 0; i++)
+            //{
+            //    n = Convert.ToInt32(Console.ReadLine());
+            //    if (n < 0)
+            //    {
+            //        Console.WriteLine("Musbet eded yaz");
+                    
+            //    }
+
+            //    else if (n == 0)
+            //    {
+            //        Console.WriteLine("0 hec bir edede bolunmur");
+                    
+            //    }
+
+            //    else if (n % 3 == 0 && n % 7 == 0)
+            //    {
+            //        Console.WriteLine("Eded 3-e ve 7-ye bolunur");
+            //        break;
+            //    }
+
+            //    else
+            //    {
+            //        Console.WriteLine("Eded 3-e ve 7-ye bolunmur");
+                    
+            //    }
+
+            //    Console.WriteLine("<<< Ekrana yeniden bir reqem yaz >>>>>>");
+            //}
+
+        }
+    }
+}
